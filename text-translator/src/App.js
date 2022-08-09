30b38a26603ed744ece69f23f44391ea7f88e175
Christopher Okoro <65927932+christofa@users.noreply.github.com>
2022-08-09 13:26:35 +0100

    Add files via upload

---
+++ text-translator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Translate from './components/Translate'
+
+function App() {
+  return (
+    <div className="App">
+      <Translate/>
+    </div>
+  );
+}
+
+export default App;
